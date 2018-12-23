fdb6d768765cbd00cf994024d422020f7426f586
unknown <mleyb@users.noreply.github.com>
2018-12-23 11:43:56 +0000

    Create ubuntu instance

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "base_security_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["1.2.3.4/32"]
+    cidr_blocks = ["1.2.3.4/32"] // replace with my own IP
   }
 
   egress {
@@ -18,5 +18,17 @@ resource "aws_security_group" "base_security_group" {
 
   tags {
     name = "base_security_group"
+  }
+}
+
+resource "aws_instance" "dev" {
+  ami                         = "${var.ami}"
+  instance_type               = "${var.aws_instance_type}"
+  key_name                    = "${aws_key_pair.admin_key.key_name}"
+  security_groups             = ["${aws_security_group.base_security_group.name}"]
+  associate_public_ip_address = true
+
+  tags {
+    name = "Ubuntu launched by Terraform"
   }
 }
--- variables.tf
+++ variables.tf
@@ -11,5 +11,12 @@ variable "aws_region" {
   description = "AWS Region"
 }
 
-variable "aws_ssh_admin_key_file" {
+variable "aws_ssh_admin_key_file" {}
+
+variable "ami" {
+  default = "ami-ee6b189d"
+}
+
+variable "aws_instance_type" {
+  default = "t2.micro"
 }
